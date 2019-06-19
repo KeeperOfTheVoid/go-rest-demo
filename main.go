b569a329146f1b8c4d48e4dd25f33c7a0d629b12
Matthew Lalmansingh <gpjazzplayer@gmail.com>
2019-06-18 20:43:22 -0500

    Add Endpoints for GET Methods
    - Add Endpoints for `getBooks` and `getBook`

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	// "encoding/json"
+	"encoding/json"
 	"log"
 	"net/http"
 	// "math/rand"
@@ -19,18 +19,36 @@ type Book struct {
 
 // Author Struct
 type Author struct {
-	Firstname		string	`json:firstname`
-	Lastname		string	`json:lastname`
+	Firstname		string	`json:"firstname"`
+	Lastname		string	`json:"lastname"`
 }
+
+/*
+ * A slice is a variable length array
+ * A regular array has to have a defined length
+ */
+// Init books variable as a slice Book struct
+var books []Book
 
 // Get All Books
 func getBooks(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(books)
 }
 
 // Get Single Book
 func getBook(w http.ResponseWriter, r *http.Request) {
-	
+	w.Header().Set("Content-Type", "application/json")
+	// Get parameters
+	params := mux.Vars(r) 
+	// Loop through books and find with ID
+	for _, item := range books {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Book{})
 }
 
 // Create a New Book
@@ -51,6 +69,10 @@ func deleteBook(w http.ResponseWriter, r *http.Request) {
 func main()  {
 	// Init Mux Router
 	router := mux.NewRouter()
+
+	// Mock Data
+	books = append(books, Book{ID: "1", Isbn: "448743", Title: "The Count of Monte Cristo", Author: &Author{Firstname: "Alexander", Lastname: "Dumas"}})
+	books = append(books, Book{ID: "2", Isbn: "847564", Title: "Pride and Prejudice", Author: &Author{Firstname: "Jane", Lastname: "Austen"}})
 
 	// Route Handlers / Endpoints
 	router.HandleFunc("/api/books", getBooks).Methods("GET")
